f6da925e7508c73c409e91f3e44916b45dea1eab
nick <nick@nicks-Air.station>
2024-07-23 11:47:14 +0200

    Add Cocktail Data Feature

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import express from "express"
 import axios from "axios"
 
 //api url
-const api_url = "www.thecocktaildb.com/api/json/v1/1";
+const api_url = "https:www.thecocktaildb.com/api/json/v1/1"; // you need to make sure to add https: to the beggining of your api url
 
 // 2. Create an express app and set the port number.
-const app = express();
+const app = express(); 
 const port = 3000;
 
 app.use(express.static("public")); // You need this to serve static files, or in other words this, you cant load custom css without this 
@@ -21,10 +21,12 @@ app.get("/",(req,res)=>{
 app.get("/get-data",async(req,res)=>{
     try {
         const response = await axios.get(api_url + "/random.php");
-        console.log(response);
-        res.render({content:response.data});
-    } catch (error) {
+        console.log(response.data); // you need to see output of this 
+        //res.render({content:response.data}); // change after testing the line above
+        res.json(response.data);
         
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
 })
 //Check to see if port is working 
@@ -34,16 +36,3 @@ app.listen(port, ()=>{
 
 
 
-app.get("/", (req, res) => {
-    res.render("index.ejs", { content: "Waiting for data..." });
-  });
-  
-  app.post("/get-secret", async (req, res) => {
-    const searchId = req.body.id;
-    try {
-      const result = await axios.get(API_URL + "/secrets/" + searchId, config);
-      res.render("index.ejs", { content: JSON.stringify(result.data) }); // the returned data is shown but in a strigified format 
-    } catch (error) {
-      res.render("index.ejs", { content: JSON.stringify(error.response.data) });
-    }
-  });
---
+++ public/script.js
@@ -0,0 +1,19 @@
+//Instead of importing axios as you usally do inside your sever, you need to import axios using the cdn into your index.ejs file isntead, this will load axios globally.
+const fetch_data_button = document.getElementById("fetchData"); // this is the button to get random cocktails
+fetch_data_button.style.backgroundColor = "green"; // this is here to test to see if the button is correctly connected 
+console.log("Button Script loaded"); // this is to test to see if this works properly 
+
+//This code below will execute a request, when a button is clicked on the client side 
+fetch_data_button.addEventListener("click",async()=>{
+    try {
+        const response = await axios.get("/get-data"); // connect to the localhost this compueter, to the get-data endpoint on the server
+        //console.log("triggerd");
+        console.log(response.data);
+        const cocktail = response.data.drinks[0];
+
+    } catch (error) {
+        console.error("Error fetching data:", error);   
+    }
+});
+
+/// try to get background color for button to some color eg: green red blue etc:
