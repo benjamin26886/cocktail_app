3f0a17772fbe272e99b113a247c6ea7c82ffa4dc
nick <nick@nicks-Air.lan>
2024-07-29 15:06:41 +0200

    Add instructions to card

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express"
 import axios from "axios"
 
 var drink_name = null;
+var instructions = null;
 
 //api url
 const api_url = "https:www.thecocktaildb.com/api/json/v1/1"; // you need to make sure to add https: to the beggining of your api url
@@ -16,7 +17,7 @@ app.use(express.static("public")); // You need this to serve static files, or in
 //get random data 
 app.get("/",(req,res)=>{
     //let randomWord = "cheese";
-    res.render("index.ejs",{name_of_drink:drink_name});
+    res.render("index.ejs",{name_of_drink:drink_name, instructions:instructions});
 })
 
 
@@ -24,10 +25,12 @@ app.get("/",(req,res)=>{
 app.get("/get-data",async(req,res)=>{
     try {
         const response = await axios.get(api_url + "/random.php");
-        //console.log(response.data); // you need to see output of this, turn on if you need to see the whole object
+        ; // you need to see output of this, turn on if you need to see the whole object
         drink_name = response.data.drinks[0].strDrink; // this is to see if you can acces the drink name in the json object
+        instructions = response.data.drinks[0].strInstructions;
         console.log(drink_name);
-
+        console.log(instructions);
+        //console.log(response.data)
        // res.render("index.ejs",{name_of_drink:drink_name}); // change after testing the line above
         //res.render("index.ejs",{name_of_drink:drink_name});
         res.redirect("/");
